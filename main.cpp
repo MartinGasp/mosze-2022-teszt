5c8216d1ed46166704d7df7ada016a2edfe169a8
MartinGasp <gaspar.martin69@gmail.com>
2022-09-19 17:59:12 +0200

    kommentelve a hibák

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS]; // N_ELEMENTS rosszul van beíírva
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    for (int i = 0;) // hiányos for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // rosszul van kitöltve
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // nem írja ki a számot
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; //nincs kezdő érték megadva az átlag változónak
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
