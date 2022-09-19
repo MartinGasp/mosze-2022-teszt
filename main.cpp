7496b11ad8f3189e098c6c69571c21010296e386
MartinGasp <gaspar.martin69@gmail.com>
2022-09-19 18:11:45 +0200

    kiírás javítása

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[N_ELEMENTS];
-    std::cout << '1-100 ertekek duplazasa' << std::endl;
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     for (int i = 0;i<=N_ELEMENTS;i++)
     {
         std::cout << "Ertek:" << b[i] = i * 2 << std::endl;
