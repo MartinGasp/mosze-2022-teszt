dde40823c1486053f83a636c4214a46d37920961
MartinGasp <gaspar.martin69@gmail.com>
2022-09-19 18:16:17 +0200

    bugfix

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
         std::cout << "Ertek:" << b[i] << std::endl;
     }   
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag=0;
+    double atlag=0;
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         atlag += b[i];
